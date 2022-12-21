b5bc883c1db5b4947db555d7c745a0cae668bfbc
Jeff Schuman <jschuman@solutionstreet.com>
2022-12-21 17:07:23 -0500

    setup stats

--- src/components/TrafficSignal.js
+++ src/components/TrafficSignal.js
@@ -3,6 +3,11 @@ import LightImage from './LightImage';
 
 const TrafficSignal = () => {
   let [currentLight, setCurrentLight] = useState("Red");
+  let [lightStats, setLightStats] = useState({
+    "Red": 0,
+    "Green": 0,
+    "Yellow": 0
+  })
 
   const cycle = {
     "Red": "Green",
@@ -11,7 +16,8 @@ const TrafficSignal = () => {
   }
 
   const nextLightCycle = () => {
-    setCurrentLight(cycle[currentLight]);
+    let nextLight = cycle[currentLight];
+    setCurrentLight(nextLight);
   }
 
   const forceLight = (light) => {
@@ -23,7 +29,7 @@ const TrafficSignal = () => {
       <LightImage light={currentLight} clickHandler={nextLightCycle} />
       <hr />
       <button class='action' onClick={() => forceLight("Red")}>Make Red</button>
-      <button class='action' onClick={() => forceLight("Green")}>Make Green</button>
+      <button class='action' onClick={() => forceLight("Red")}>Make Green</button>
       <button class='action' onClick={() => forceLight("Yellow")}>Make Yellow</button>
     </div>
   )
